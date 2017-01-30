6980f68f4ae12d8307398102cd4da222d6e1f9ce
tapie <tapie51@gmail.com>
2017-01-30 13:18:15 -0500

    cambio DatePicker
    commit DatePicker

--- app/src/main/java/etg/com/petagram/MainActivity.java
+++ app/src/main/java/etg/com/petagram/MainActivity.java
@@ -1,9 +1,15 @@
 package etg.com.petagram;
 
+import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.content.Intent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import android.os.Build;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,15 +17,31 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.DatePicker;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Contact contact;
+    DatePickerDialog fechaNacimiento;
+    TextInputEditText tvFechaNacimiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         contact = new Contact();
+        Calendar newCalendar = Calendar.getInstance();
+        final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        tvFechaNacimiento = (TextInputEditText) findViewById(R.id.txtBirthdate);
+        tvFechaNacimiento.setOnClickListener(this);
+        fechaNacimiento = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                tvFechaNacimiento.setText(sdf.format(newDate.getTime()));
+            }
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
         Bundle params = getIntent().getExtras();
         if (params != null) {
             if (!params.getString(getResources().getString(R.string.pProcess)).isEmpty()) {
@@ -34,6 +56,22 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             }
+        }
+    }
+
+    public void onClick(View v) {
+        if (v == tvFechaNacimiento) {
+            /*DialogFragment fragment = new DatePickerFragment();
+            fragment.show(getFragmentManager(), "datePicker");*/
+            if (contact != null) {
+                if (contact.getBirthDate() != null) {
+                    if (contact.getBirthDate().contains("/")) {
+                        String fecha[] = contact.getBirthDate().split("/");
+                        fechaNacimiento.updateDate(Integer.valueOf(fecha[2]), Integer.valueOf(fecha[1]), Integer.valueOf(fecha[0]));
+                    }
+                }
+            }
+            fechaNacimiento.show();
         }
     }
 
